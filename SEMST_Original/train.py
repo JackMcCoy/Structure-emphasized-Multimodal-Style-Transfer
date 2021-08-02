a6a0cc024780c546d26fcb80225c19302a725f99
David Gill <davegill@Davids-MBP.home>
2021-08-01 20:41:20 -0400

    return path and image

--- SEMST_Original/train.py
+++ SEMST_Original/train.py
@@ -61,7 +61,7 @@ class FlatFolderDataset(data.Dataset):
         path = self.paths[index]
         img = Image.open(str(path)).convert('RGB')
         img = self.transform(img)
-        return img
+        return path, img
 
     def __len__(self):
         return len(self.paths)
